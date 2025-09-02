e41eb339b0df7801a8c5ebde7b61dcc3906f2cf8
Sofia Brown <sofiabrown1003@gmail.com>
2025-09-02 22:03:55 +0000

    I need thesame fonyts too

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased">
+      <body className="antialiased">
         {children}
         <Toaster />
       </body>
