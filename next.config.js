aee918c41565c272ba361ecd0a314bc3076cd2b8
Sofia Brown <sofiabrown1003@gmail.com>
2025-09-04 20:12:20 +0000

    image of the meme should show o this page too

--- next.config.js
+++ next.config.js
@@ -26,6 +26,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.imgflip.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
